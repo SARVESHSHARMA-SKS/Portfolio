89d3285b403b5cf1468ea4c8ba89497294ca0a80
Sarvesh Sharma <sssarvesh1999@gmail.com>
2021-10-22 18:26:16 +0530

    Change in the Font Size

--- components/Project.jsx
+++ components/Project.jsx
@@ -9,10 +9,20 @@ const Projects = () => {
         <>
         <h1 style={{textAlign:"center",marginTop:"30px",  color:"#5E5757"}}>PROJECTS</h1>
         <div className={styles.outer__div }>
-            <div>
+            <div >
                 <img src="https://i.postimg.cc/rpwMHBmq/landingpagesst-1.png"  alt="makemytrip"/>
-                <h3>Clone of MakeMyTrip</h3>
-                <p>MakeMyTrip is an Indian online travel company founded in 2000.Headquartered in Gurugram, Haryana, the company provides online travel services including flight tickets, domestic and international holiday packages, hotel reservations, rail, and bus tickets</p>
+                <h3 >Clone of MakeMyTrip</h3>
+                    <p>
+                        <p style={{ textAlign: "center" }}> MakeMyTrip is an Indian online travel company.</p>
+                        
+                        <p style={{ textAlign: "justify" }}> We have implemented the search on the basis of stoppage, flight company, location. </p>
+                        
+                         Tech Stack: React.js | Node.js | Express | MongoDB | CSS | JAVASCRIPT
+                        <br/>
+                        Collaborative Project | 4-member team | Project
+                         <br/>
+                        Duration : 5 Days
+                    </p>
                 <a href="https://github.com/nitish14920/MakeMyTrip" target="_blank" rel="noopener noreferrer" >
                   <button>Github</button>
                 </a>
@@ -27,10 +37,22 @@ const Projects = () => {
             <div>
                 <img src="https://i.postimg.cc/bvjrCyDW/0-0-0-0-1-Document-Google-Chrome-28-08-2021-09-13-39.png"  alt="Couch"/>
                 <h3>Clone of InternShala</h3>
-                <p>Internshala is an internship and online training platform, based in Gurgaon, India.Founded by Sarvesh Agrawal, an IIT Madras alumnus, in 2011, the website helps students find internships with organisations in India</p>
-
+                    <p>
+                        <p style={{textAlign:"center"}}>Internshala is an internship listing and online
+                        training platform.
+                        </p>
+        
+                       <p style={{textAlign:"justify"}}>  We have added location, domain-based filtering and internship searching feature on Internshala Clone Website.</p>
+                        
+Tech Stack: HTML | CSS | Javascript | Node |
+                        MongoDB| MySql | DOM |
+                          <br/>
+                        Collaborative Project | 4-member team | Project
+                          <br/>
+   Duration : 2 Weeks
+                </p>
               
-               <a href="https://github.com/SrikanthReddyBV/Internshala" target="_blank" rel="noopener noreferrer">
+               <a href="https://blooming-bastion-29120.herokuapp.com/pages/index.ejs" target="_blank" rel="noopener noreferrer">
                 
               <button>Github</button>
                 </a>
